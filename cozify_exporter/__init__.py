8398dbc7d4403b2fbaa7eeba36bbe1ba568e6c32
Jinna Kiisuo <jinna+git@nocturnal.fi>
2023-09-09 15:07:22 +0300

    Actually implement hub_info metric, add error metrics

--- cozify_exporter/__init__.py
+++ cozify_exporter/__init__.py
@@ -2,10 +2,10 @@
 import time, signal, sys, os
 from absl import logging, app, flags
 
-from cozify import hub, cloud
+from cozify import hub, cloud, hub_api
 from cozify.Error import APIError
 
-from prometheus_client import start_http_server, Summary
+from prometheus_client import start_http_server, Info, Counter, Gauge
 
 from cozify_exporter.device_metrics import process_devices
 
@@ -18,9 +18,12 @@ flags.DEFINE_integer('interval', os.getenv(f'{PREFIX}_INTERVAL', 60), 'Interval 
 flags.DEFINE_integer('tolerance_read', 10, 'How many consecutive read errors are fine', lower_bound=0)
 
 
-# Define metrics
+# Define top level metrics
 PREFIX = 'cozify_'
-HUB_INFO = Summary(PREFIX + 'hub_info', 'General Hub into', ['host', 'version', 'mac'])
+HUB_INFO = Info(PREFIX + 'hub_info', 'General Hub into')
+ERRORS_CONSECUTIVE = Gauge(PREFIX + 'errors_consecutive', 'Data fetch errors in a row')
+ERRORS_TOTAL = Counter(PREFIX + 'errors_total', 'Data fetch errors in total')
+QUERY_TOTAL = Counter(PREFIX + 'query_total', 'Data fetches performed in total')
 
 
 def main(argv):
@@ -29,6 +32,7 @@ def main(argv):
 
     start_http_server(FLAGS.port)
     logging.info(f'Serving metrics at :{FLAGS.port}/metrics')
+    ERRORS_CONSECUTIVE.set(0)
 
     while True:
         try:
@@ -38,18 +42,31 @@ def main(argv):
             # every 28 days.
             cloud.ping()
 
-            # Get all device data
-            data = hub.devices()
+            hub_id = hub.default()
+            host = hub.host(hub_id)
+            info = hub_api.hub(host=host)
+            HUB_INFO.info({
+                'id': hub_id,
+                'host': host,
+                'name': info['name'],
+                'version': info['version']
+                })
+
+            devices = hub.devices()
         except APIError as e:
             error_counter_read += 1
+            ERRORS_CONSECUTIVE.inc()
+            ERRORS_TOTAL.inc()
             logging.error(f'Failed to get data({error_counter_read}/{FLAGS.tolerance_read}). Error code: {e.status_code}, error: {e}')
             if error_counter_read > FLAGS.tolerance_read:
                 raise # Too many errors, let it burn to the ground
         else: # data retrieval succeeded
             error_counter_read = 0
+            ERRORS_CONSECUTIVE.set(0)
 
-        process_devices(data)
+        process_devices(devices)
 
+        QUERY_TOTAL.inc()
         time.sleep(FLAGS.interval)
 
 def run():
